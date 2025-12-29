878a1c104dca793786dc40b3c384477d0385bcba
Nozomi Hosaka <hosaka.non.work@gmail.com>
2025-12-29 14:54:27 +0900

    Revert "feat(attachments): add search_attachments tool"

--- src/attachments/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Attachments domain exports
- */
-export * from "./types.js";
-export * from "./schemas.js";
-export * from "./client.js";
-export * from "./handlers.js";
-export * from "./tools.js";
